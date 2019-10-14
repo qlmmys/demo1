ee0f0ebcda8967398a0f1dc93f12d54bf3d63b1f
test <test@liepin.com>
2019-10-14 20:15:07 +0800

    e

--- src/main/java/com/example/demo/part1/Test1.java
+++ src/main/java/com/example/demo/part1/Test1.java
@@ -9,8 +9,6 @@ package com.example.demo.part1;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("c");
-        System.out.println("d");
-        System.out.println("e");
+        System.out.println("a");
     }
 }
