162b91069a59d4e4382f1da2aadd1f35d2584a9a
qimeng1 <qimeng1@liepin.com>
2019-10-14 15:42:29 +0800

    测试插件

---
+++ src/main/java/com/liepin/TestJosn.java
@@ -0,0 +1,34 @@
+package com.liepin;
+
+/**
+ * 描述：
+ *
+ * @author qimeng
+ * @createTime 2019年10月14日 14:50:00
+ */
+public class TestJosn {
+
+    /**
+     * success : true
+     * code : 1
+     */
+
+    private boolean success;
+    private int code;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
