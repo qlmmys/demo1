2b44004c84be83d432c02f071d62aad627b4af8e
test <test@liepin.com>
2019-10-14 19:37:51 +0800

    init

---
+++ src/main/java/com/example/demo/part1/Test1.java
@@ -0,0 +1,14 @@
+package com.example.demo.part1;
+
+/**
+ * 描述：
+ *
+ * @author qimeng
+ * @createTime 2019年10月14日 17:05:00
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
