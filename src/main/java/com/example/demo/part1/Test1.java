5f320a7c800de6fd5646578afbc34fadf2d493b2
test <test@liepin.com>
2019-10-15 15:27:42 +0800

    3

--- src/main/java/com/example/demo/part1/Test1.java
+++ src/main/java/com/example/demo/part1/Test1.java
@@ -9,6 +9,6 @@ package com.example.demo.part1;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("第1次修改");
+        System.out.println("第3次修改");
     }
 }
