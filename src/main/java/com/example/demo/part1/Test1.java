f570b906ac04197b55373684e68e499421425a68
qimeng1 <qimeng1@liepin.com>
2019-10-15 15:23:22 +0800

    1

--- src/main/java/com/example/demo/part1/Test1.java
+++ src/main/java/com/example/demo/part1/Test1.java
@@ -9,8 +9,6 @@ package com.example.demo.part1;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("第一次修改");
-        System.out.println("d");
-        System.out.println("e");
+        System.out.println("第1次修改");
     }
 }
